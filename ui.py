888c465a69d4f83a7867d3977afcf7ebc34100ff
Goodlitch <57591124+Goodlitch@users.noreply.github.com>
2019-11-20 17:16:54 +0300

    Всё закомментировано и отсортировано для удобства чтения.

--- ui.py
+++ ui.py
@@ -12,94 +12,84 @@ class UI():
         window.setMinimumSize(500, 300)
         window.setMaximumSize(500, 300)
 
-        # Указание размера шрифта для всех элементов окна.
-        self.font_size = QFont()
-        self.font_size.setPointSize(15)
-        window.setFont(self.font_size)
+        # Создание виджета и горизонтальной сеточной разметки в нём.
+        self.HLW = QWidget(window)
+        self.HLW.setGeometry(0, 255, 500, 45)
+
+        self.HL = QHBoxLayout(self.HLW)
+        self.HL.setContentsMargins(5, 0, 5, 5)
+        self.HL.setSpacing(5)
 
         # Создание кнопки, открывающей окно выбора файла.
         self.open_button = QPushButton(window)
         self.open_button.setGeometry(5, 5, 118.75, 40)
         self.open_button.setText('Open')
-
-        #
-        self.HLW = QWidget(window)
-        self.HLW.setGeometry(0, 255, 500, 45)
-        self.HL = QHBoxLayout(self.HLW)
-        self.HL.setContentsMargins(5, 0, 5, 5)
-        self.HL.setSpacing(5)
-
         # Создание кнопки, отвечающей за включение/паузу проигрывания аудио.
         self.play_pause_button = QPushButton(self.HLW)
         self.HL.addWidget(self.play_pause_button)
         self.play_pause_button.setEnabled(False)
         self.play_pause_button.setText('Play')
-
         # Создание кнопки, отвечающей за полную остановку проигрывания аудио.
         self.stop_button = QPushButton(self.HLW)
-        self.stop_button.setEnabled(False)
         self.HL.addWidget(self.stop_button)
+        self.stop_button.setEnabled(False)
         self.stop_button.setText('Stop')
-
-        # Ползунок, который будет показывать позицию трека и позволять её
-        # изменять.
-        self.position_slider = QSlider(window)
-        self.position_slider.setEnabled(False)
-        self.position_slider.setOrientation(Qt.Horizontal)
-        self.position_slider.setGeometry(5, 225, 490, 25)
-
-        #
-        self.font_mono = QFont()
-        self.font_mono.setFamily('Monospace')
-
-        #
-        self.position_label = QLabel(window)
-        self.position_label.setEnabled(False)
-        self.position_label.setGeometry(5, 200, 75, 20)
-        self.position_label.setFont(self.font_mono)
-        self.position_label.setText('00:00')
-
-        #
-        self.duration_label = QLabel(window)
-        self.duration_label.setEnabled(False)
-        self.duration_label.setGeometry(420, 200, 75, 20)
-        self.duration_label.setAlignment(Qt.AlignRight)
-        self.duration_label.setFont(self.font_mono)
-        self.duration_label.setText('00:00')
-
-        #
-        self.volume_slider = QSlider(window)
-        self.volume_slider.setToolTip('Volume')
-        self.volume_slider.setEnabled(False)
-        self.volume_slider.setMaximum(100)
-        self.volume_slider.setSliderPosition(100)
-        self.volume_slider.setGeometry(470, 50, 25, 145)
-
-        #
+        # Пробел для выравнивания разметки.
         self.space = QSpacerItem(118.75, 40)
         self.HL.addItem(self.space)
-
         # Создание кнопки, открывающей окно настроек.
         self.settings_button = QPushButton(self.HLW)
         self.HL.addWidget(self.settings_button)
         self.settings_button.setText('Settings')
 
-        #
-        self.font_artist = QFont()
-        self.font_artist.setPointSize(10)
+        # Cоздание текстового ярлыка с информацией о позиции проигрывания трека.
+        self.position_label = QLabel(window)
+        self.position_label.setGeometry(5, 200, 75, 20)
+        self.position_label.setEnabled(False)
+        self.position_label.setText('00:00')
+        # Cоздание текстового ярлыка с информацией о длительности трека.
+        self.duration_label = QLabel(window)
+        self.duration_label.setGeometry(420, 200, 75, 20)
+        self.duration_label.setAlignment(Qt.AlignRight)
+        self.duration_label.setEnabled(False)
+        self.duration_label.setText('00:00')
 
-        #
+        # Ползунок, который показывает позицию трека и позволять её изменять.
+        self.position_slider = QSlider(window)
+        self.position_slider.setGeometry(5, 225, 490, 25)
+        self.position_slider.setOrientation(Qt.Horizontal)
+        self.position_slider.setEnabled(False)
+        # Ползунок для изменения громкости.
+        self.volume_slider = QSlider(window)
+        self.volume_slider.setGeometry(470, 50, 25, 145)
+        self.volume_slider.setMaximum(100)
+        self.volume_slider.setSliderPosition(100)
+        self.volume_slider.setEnabled(False)
+        self.volume_slider.setToolTip('Volume')
+
+        # Cоздание текстового ярлыка с информацией об авторе трека.
         self.artist_label = QLabel(window)
         self.artist_label.setGeometry(127.75, 5, 367.25, 15)
         self.artist_label.setAlignment(Qt.AlignRight)
-        self.artist_label.setFont(self.font_artist)
-
-        #
-        self.font_title = QFont()
-        self.font_title.setPointSize(15)
-
-        #
+        # Cоздание текстового ярлыка с информацией о название трека.
         self.title_label = QLabel(window)
         self.title_label.setGeometry(127.75, 20, 367.25, 25)
         self.title_label.setAlignment(Qt.AlignRight)
-        self.title_label.setFont(self.font_title)
+
+        # Указание размера шрифта для окна и его элементов.
+        font = QFont()
+        font.setPointSize(15)
+        window.setFont(font)
+
+        font = QFont()
+        font.setFamily('Monospace')
+        self.position_label.setFont(font)
+        self.duration_label.setFont(font)
+
+        font = QFont()
+        font.setPointSize(10)
+        self.artist_label.setFont(font)
+
+        font = QFont()
+        font.setPointSize(15)
+        self.title_label.setFont(font)
